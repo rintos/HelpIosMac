92b1d76e06e8af6ff4a2abb0e4f6a48c314664a3
VictorSantoryu <victor.santoryu@github.com>
2019-08-28 20:29:05 -0300

    Initial Commit

---
+++ HelpIosMac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelpIosMac
+//
+//  Created by Victor on 28/08/2019.
+//  Copyright © 2019 Rinver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
